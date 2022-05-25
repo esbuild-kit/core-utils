bc691a1b8c00a6e25b07dac51902c47cd4071243
hiroki osame <hiroki.osame@gmail.com>
2022-05-24 20:51:45 -0400

    feat: resolve-ts-path helper function (#8)
    * feat: resolve-ts-path helper function
    
    * feat: resolve-ts-path helper function
    
    * feat: resolve-ts-path helper function

---
+++ src/resolve-ts-path.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+
+const tsExtensions: Record<string, string> = {
+	'.js': '.ts',
+	'.cjs': '.cts',
+	'.mjs': '.mts',
+};
+
+export function resolveTsPath(filePath: string) {
+	const extension = path.extname(filePath);
+	const tsExtension = tsExtensions[extension];
+
+	if (tsExtension) {
+		return filePath.slice(0, -extension.length) + tsExtension;
+	}
+}
