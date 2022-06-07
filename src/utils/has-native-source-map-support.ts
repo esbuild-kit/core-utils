bbda533f86b0da5bcc229450ac933e96379af4f4
Dylan Piercey <dpiercey@ebay.com>
2022-06-07 13:37:47 -0700

    fix: native source maps will not work if prepareStackTrace overwritten

--- src/utils/has-native-source-map-support.ts
+++ src/utils/has-native-source-map-support.ts
@@ -1,1 +1,7 @@
-export const hasNativeSourceMapSupport = 'setSourceMapsEnabled' in process;
+// Check if native source maps are supported by seeing if the programatic api is available.
+// https://nodejs.org/dist/latest-v18.x/docs/api/process.html#processsetsourcemapsenabledval
+
+// Also check if `Error.prepareStackTrace` has been set as that makes native source maps not work.
+// https://nodejs.org/dist/latest-v18.x/docs/api/cli.html#:~:text=Overriding%20Error.prepareStackTrace%20prevents%20%2D%2Denable%2Dsource%2Dmaps%20from%20modifying%20the%20stack%20trace.
+
+export const hasNativeSourceMapSupport = !Error.prepareStackTrace && 'setSourceMapsEnabled' in process;
