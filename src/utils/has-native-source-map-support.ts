d9049bb8dc4c60af25e45661a21bf57b2067fadc
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-16 22:22:52 -0400

    refactor

--- src/utils/has-native-source-map-support.ts
+++ src/utils/has-native-source-map-support.ts
@@ -1,7 +1,13 @@
-// Check if native source maps are supported by seeing if the programatic api is available.
-// https://nodejs.org/dist/latest-v18.x/docs/api/process.html#processsetsourcemapsenabledval
+export const hasNativeSourceMapSupport = (
+	/**
+	 * Check if native source maps are supported by seeing if the api is available
+	 * https://nodejs.org/dist/latest-v18.x/docs/api/process.html#processsetsourcemapsenabledval
+	 */
+	'setSourceMapsEnabled' in process
 
-// Also check if `Error.prepareStackTrace` has been set as that makes native source maps not work.
-// https://nodejs.org/dist/latest-v18.x/docs/api/cli.html#:~:text=Overriding%20Error.prepareStackTrace%20prevents%20%2D%2Denable%2Dsource%2Dmaps%20from%20modifying%20the%20stack%20trace.
-
-export const hasNativeSourceMapSupport = !Error.prepareStackTrace && 'setSourceMapsEnabled' in process;
+	/**
+	 * Overriding Error.prepareStackTrace prevents --enable-source-maps from modifying the stack trace
+	 * https://nodejs.org/dist/latest-v18.x/docs/api/cli.html#:~:text=Overriding%20Error.prepareStackTrace%20prevents%20%2D%2Denable%2Dsource%2Dmaps%20from%20modifying%20the%20stack%20trace.
+	 */
+	&& !('prepareStackTrace' in Error)
+);
