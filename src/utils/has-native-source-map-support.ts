e901698c123e04541c16e903bf16c9b49e2d6da5
Dylan Piercey <pierceydylan@gmail.com>
2022-06-16 19:41:27 -0700

    fix(source-maps): disable native source-maps if `Error.prepareStackTrace` is customized (#13)
    * fix: native source maps will not work if prepareStackTrace overwritten
    
    * refactor
    
    Co-authored-by: Hiroki Osame <hiroki.osame@gmail.com>

--- src/utils/has-native-source-map-support.ts
+++ src/utils/has-native-source-map-support.ts
@@ -1,1 +1,13 @@
-export const hasNativeSourceMapSupport = 'setSourceMapsEnabled' in process;
+export const hasNativeSourceMapSupport = (
+	/**
+	 * Check if native source maps are supported by seeing if the api is available
+	 * https://nodejs.org/dist/latest-v18.x/docs/api/process.html#processsetsourcemapsenabledval
+	 */
+	'setSourceMapsEnabled' in process
+
+	/**
+	 * Overriding Error.prepareStackTrace prevents --enable-source-maps from modifying the stack trace
+	 * https://nodejs.org/dist/latest-v18.x/docs/api/cli.html#:~:text=Overriding%20Error.prepareStackTrace%20prevents%20%2D%2Denable%2Dsource%2Dmaps%20from%20modifying%20the%20stack%20trace.
+	 */
+	&& !('prepareStackTrace' in Error)
+);
