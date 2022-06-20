8171bb547830b6fb9249afa6c8bc352adcc433c8
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-20 14:34:24 -0400

    refactor

---
+++ src/transform/apply-transformers.ts
@@ -0,0 +1,93 @@
+import remapping from '@ampproject/remapping';
+import type { SourceMapInput } from '@ampproject/remapping';
+
+type MaybePromise<T> = T | Promise<T>;
+
+type IntersectionArray<T extends unknown[]> = (
+	T extends [infer FirstElement, ...infer RestElements]
+		? FirstElement & IntersectionArray<RestElements>
+		: {}
+);
+
+type Transformed = {
+	code: string;
+	map: SourceMapInput;
+};
+
+type TransformerResult = Transformed | undefined;
+
+type Transformer<
+	Result extends MaybePromise<TransformerResult> = TransformerResult
+> = (code: string) => Result;
+
+type Results<
+	Arr extends Transformer<MaybePromise<TransformerResult>>[]
+> = IntersectionArray<{
+	[Key in keyof Arr]: (
+		Arr[Key] extends Transformer<infer ReturnType>
+			? ReturnType
+			: unknown
+	);
+}>;
+
+type AddSourceMap<T> = Omit<T, 'map'> & { map: string };
+
+export function applyTransformersSync<
+	T extends Readonly<Transformer[]>,
+>(
+	code: string,
+	transformers: T,
+) {
+	const maps: SourceMapInput[] = [];
+	const result = {
+		code,
+		map: '',
+	};
+
+	for (const transformer of transformers) {
+		const transformed = transformer(result.code);
+
+		if (transformed) {
+			Object.assign(result, transformed);
+			maps.unshift(transformed.map);
+		}
+	}
+
+	result.map = (
+		maps.length > 1
+			? remapping(maps, () => null).toString()
+			: maps[0].toString()
+	);
+
+	return result as unknown as AddSourceMap<Results<[...T]>>;
+}
+
+export async function applyTransformers<
+	T extends Readonly<Transformer<MaybePromise<TransformerResult>>[]>,
+>(
+	code: string,
+	transformers: T,
+) {
+	const maps: SourceMapInput[] = [];
+	const result = {
+		code,
+		map: '',
+	};
+
+	for (const transformer of transformers) {
+		const transformed = await transformer(result.code);
+
+		if (transformed) {
+			Object.assign(result, transformed);
+			maps.unshift(transformed.map);	
+		}
+	}
+
+	result.map = (
+		maps.length > 1
+			? remapping(maps, () => null).toString()
+			: maps[0].toString()
+	);
+
+	return result as unknown as AddSourceMap<Results<[...T]>>;
+}
---
+++ src/transform/index.ts
@@ -0,0 +1,96 @@
+import { pathToFileURL } from 'url';
+import type { TransformOptions } from 'esbuild';
+import {
+	transform as esbuildTransform,
+	transformSync as esbuildTransformSync,
+	version as esbuildVersion,
+} from 'esbuild';
+import { transformDynamicImport } from './transform-dynamic-import';
+import { sha1 } from '../utils/sha1';
+import cache from './cache';
+import { applyTransformersSync, applyTransformers } from './apply-transformers';
+import { getEsbuildOptions } from './get-esbuild-options';
+import { applySourceMap } from '../source-map';
+
+export * from './transform-dynamic-import';
+
+// Used by cjs-loader
+export function transformSync(
+	code: string,
+	filePath: string,
+	extendOptions?: TransformOptions,
+	sourcemaps?: Map<string, string>,
+): string {
+	const define: { [key: string]: string } = {};
+
+	if (!(filePath.endsWith('.cjs') || filePath.endsWith('.cts'))) {
+		define['import.meta.url'] = `'${pathToFileURL(filePath)}'`;
+	}
+
+	const esbuildOptions = getEsbuildOptions({
+		sourcefile: filePath,
+		define,
+		...extendOptions,
+	});
+
+	const hash = sha1(code + JSON.stringify(esbuildOptions) + esbuildVersion);
+	const cacheHit = cache.get(hash);
+
+	if (cacheHit) {
+		return applySourceMap(cacheHit, filePath, sourcemaps);
+	}
+
+	const transformed = applyTransformersSync(
+		code,
+		[
+			(code) => esbuildTransformSync(code, esbuildOptions),
+			transformDynamicImport,
+		] as const,
+	);
+
+	if (transformed.warnings.length > 0) {
+		const { warnings } = transformed;
+		for (const warning of warnings) {
+			console.log(warning);
+		}
+	}
+
+	cache.set(hash, transformed);
+
+	return applySourceMap(transformed, filePath, sourcemaps);
+}
+
+// Used by esm-loader
+export async function transform(
+	code: string,
+	filePath: string,
+	extendOptions?: TransformOptions,
+	sourcemaps?: Map<string, string>,
+): Promise<string> {
+	const esbuildOptions = getEsbuildOptions({
+		sourcefile: filePath,
+		...extendOptions,
+	});
+
+	const hash = sha1(code + JSON.stringify(esbuildOptions) + esbuildVersion);
+	const cacheHit = cache.get(hash);
+	if (cacheHit) {
+		return applySourceMap(cacheHit, filePath, sourcemaps);
+	}
+
+	const transformed = await applyTransformers(code, [
+		(code) => esbuildTransform(code, esbuildOptions),
+		transformDynamicImport,
+	] as const);
+
+	if (transformed.warnings.length > 0) {
+		const { warnings } = transformed;
+		for (const warning of warnings) {
+			console.log(warning);
+		}
+	}
+
+	cache.set(hash, transformed);
+
+	return applySourceMap(transformed, filePath, sourcemaps);
+}
