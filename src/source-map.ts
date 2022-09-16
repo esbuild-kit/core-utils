4eee59ee576877016927c75ce4fff74ef74bf6b5
Hiroki Osame <hiroki.osame@gmail.com>
2022-09-17 00:48:51 +0900

    fix: move native source map check to installation

--- src/source-map.ts
+++ src/source-map.ts
@@ -1,23 +1,24 @@
 import sourceMapSupport from 'source-map-support';
 import type { SourceMapInput } from '@ampproject/remapping';
 
-const hasNativeSourceMapSupport = (
-	/**
-	 * Check if native source maps are supported by seeing if the API is available
-	 * https://nodejs.org/dist/latest-v18.x/docs/api/process.html#processsetsourcemapsenabledval
-	 */
-	'setSourceMapsEnabled' in process
-
-	/**
-	 * Overriding Error.prepareStackTrace prevents --enable-source-maps from modifying the stack trace
-	 * https://nodejs.org/dist/latest-v18.x/docs/api/cli.html#:~:text=Overriding%20Error.prepareStackTrace%20prevents%20%2D%2Denable%2Dsource%2Dmaps%20from%20modifying%20the%20stack%20trace.
-	 *
-	 * https://github.com/nodejs/node/blob/91193825551f9301b6ab52d96211b38889149892/lib/internal/errors.js#L141
-	 */
-	&& typeof Error.prepareStackTrace !== 'function'
-);
-
 export function installSourceMapSupport() {
+	const hasNativeSourceMapSupport = (
+		/**
+		 * Check if native source maps are supported by seeing if the API is available
+		 * https://nodejs.org/dist/latest-v18.x/docs/api/process.html#processsetsourcemapsenabledval
+		 */
+		'setSourceMapsEnabled' in process
+
+		/**
+		 * Overriding Error.prepareStackTrace prevents --enable-source-maps from modifying
+		 * the stack trace
+		 * https://nodejs.org/dist/latest-v18.x/docs/api/cli.html#:~:text=Overriding%20Error.prepareStackTrace%20prevents%20%2D%2Denable%2Dsource%2Dmaps%20from%20modifying%20the%20stack%20trace.
+		 *
+		 * https://github.com/nodejs/node/blob/91193825551f9301b6ab52d96211b38889149892/lib/internal/errors.js#L141
+		 */
+		&& typeof Error.prepareStackTrace !== 'function'
+	);
+
 	if (hasNativeSourceMapSupport) {
 		process.setSourceMapsEnabled(true);
 		return;
