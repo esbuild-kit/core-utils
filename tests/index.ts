c9f5af472b402b62951242e651720dee04b619db
Hiroki Osame <hiroki.osame@gmail.com>
2023-08-28 17:05:23 +0900

    wip

--- tests/index.ts
+++ tests/index.ts
@@ -1,6 +1,5 @@
 import { describe } from 'manten';
 
-console.log('Node.js version', process.version);
 describe('core-utils', ({ runTestSuite }) => {
 	runTestSuite(import('./specs/transform.js'));
 	runTestSuite(import('./specs/source-maps.js'));
