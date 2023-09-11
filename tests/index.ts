0c174fad0234e948e2e8060e7fac7c78c1ae9886
Hiroki Osame <hiroki.osame@gmail.com>
2023-09-11 15:57:41 +0900

    Published branch "node-20-globalPreload"

--- tests/index.ts
+++
@@ -1,6 +0,0 @@
-import { describe } from 'manten';
-
-describe('core-utils', ({ runTestSuite }) => {
-	runTestSuite(import('./specs/transform.js'));
-	runTestSuite(import('./specs/source-maps.js'));
-});
