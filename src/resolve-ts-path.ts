5d9f24fe038a41d0eddbcf778f6590ed5eb8b745
Hiroki Osame <hiroki.osame@gmail.com>
2023-09-17 15:12:33 +0900

    fix: support query in specifier (#50)

--- src/resolve-ts-path.ts
+++ src/resolve-ts-path.ts
@@ -1,16 +1,22 @@
 import path from 'path';
 
-const tsExtensions: Record<string, string> = {
-	'.js': '.ts',
-	'.cjs': '.cts',
-	'.mjs': '.mts',
-};
+const tsExtensions: Record<string, string> = Object.create(null);
+tsExtensions['.js'] = '.ts';
+tsExtensions['.cjs'] = '.cts';
+tsExtensions['.mjs'] = '.mts';
 
-export function resolveTsPath(filePath: string) {
+export const resolveTsPath = (
+	filePath: string,
+) => {
 	const extension = path.extname(filePath);
-	const tsExtension = tsExtensions[extension];
+	const [extensionNoQuery, query] = path.extname(filePath).split('?');
+	const tsExtension = tsExtensions[extensionNoQuery];
 
 	if (tsExtension) {
-		return filePath.slice(0, -extension.length) + tsExtension;
+		return (
+			filePath.slice(0, -extension.length)
+			+ tsExtension
+			+ (query ? `?${query}` : '')
+		);
 	}
-}
+};
